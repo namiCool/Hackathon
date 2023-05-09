63e9d856ea6f8563e0282d8334498a434b6f7abc
rraj-apptio <119068600+rraj-apptio@users.noreply.github.com>
2023-05-09 11:53:46 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import 'react-router-dom';
+import Layout from './Layout';
+
+const App = () => {
+  return (<>
+    </>);
+};
+
+export default App;
---
+++ src/menuItems.js
@@ -0,0 +1,67 @@
+export const menuItems = [
+  {
+    title: 'Inform',
+    submenu: [
+      {
+        title: 'Budget Coverage',
+      },
+      {
+        title: 'Tagging Coverage',
+      },
+      {
+        title: 'Anomaly Alert Investigations',
+      },
+    ],
+  },
+  {
+    title: 'Optimize',
+    submenu: [
+      {
+        title: 'RI/SP Coverage',
+      },
+      {
+        title: 'RI/SP Vacancy',
+      },
+      {
+        title: 'IFA Storage',
+      },
+      {
+        title: 'Old Generation EBS  Usage',
+      },
+      {
+        title: 'Old Generation Compute Instance Usage',
+      },
+      {
+        title: 'Spot Instance Usage',
+      },
+      {
+        title: 'Serverless Usage',
+      },
+      {
+        title: 'Container Usage',
+      },
+    ],
+  },
+  {
+    title: 'Operate',
+    submenu: [
+      {
+        title: 'Unattached Storage',
+      },
+      {
+        title: 'Old Snapshots',
+      },
+      {
+        title: 'Idle Compute Instances',
+      },
+      {
+        title: 'Unattached Elastic IP',
+      },
+      {
+        title: 'Weeknight /\n' +
+            'Weekend Shut Down\n',
+      },
+    ],
+  },
+
+];
