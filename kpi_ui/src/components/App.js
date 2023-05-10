2fed8e0c0179b31571e8ca98ebd571c84fb98d16
naarora <naarora@apptio.com>
2023-05-10 15:08:26 +0530

    added service files

--- kpi_ui/src/components/App.js
+++ kpi_ui/src/components/App.js
@@ -1,9 +1,41 @@
 import 'react-router-dom';
-import Layout from './Layout';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import NameForm from './NameForm';
+import DropDown from './DropDown';
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render();
+
+
 
 const App = () => {
   return (<>
+  <div>
+        <h2> 
+           {" "}
+           Using the <i> React inline styles </i> to set a background image
+        </h2>
+        <br></br>
+        <div
+           class = "image"
+           style = {{
+              height: "500px",
+              width: "700px",
+               backgroundImage:
+              'url("https://cdn.shortpixel.ai/spai/ret_img/https://www.apptio.com/wp-content/uploads/Data-art-2-01.svg  ")',
+              align:'center',
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+           }}
+        >
+         <NameForm/>
+<DropDown/>
+        </div>
+     </div>
+
     </>);
 };
 
-export default App;
+
+
+export default  App;
--- kpi_ui/src/components/Dropdown.js
+++ kpi_ui/src/components/Dropdown.js
@@ -1,22 +1,49 @@
-import MenuItems from './MenuItems';
-const Dropdown = ({ submenus, dropdown, depthLevel }) => {
-  depthLevel = depthLevel + 1;
-  const dropdownClass = depthLevel > 1 ? 'dropdown-submenu' : '';
-  return (
-    <ul
-      className={`dropdown ${dropdownClass} ${
-        dropdown ? 'show' : ''
-      }`}
-    >
-      {submenus.map((submenu, index) => (
-        <MenuItems
-          items={submenu}
-          key={index}
-          depthLevel={depthLevel}
-        />
-      ))}
-    </ul>
-  );
-};
+import React from 'react';
 
-export default Dropdown;
+class DropDown extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: 'coconut'};
+      this.state1 = {value: 'coconut'};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('Selected KPI: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+          Select the category of KPI:
+            <select value={this.state.value} onChange={this.handleChange}>
+              <option value="Optimize">Optimize</option>
+              <option value="Operate">Operate</option>
+              <option value="Inform">Inform</option>
+            </select>
+          </label>
+          <br/>
+          <label>
+          Select the KPI Name:
+            <select value={this.state1.value} onChange={this.handleChange}>
+              <option value="Budget Coverage">Budget Coverage</option>
+              <option value="Tagging Coverage">Tagging Coverage</option>
+              <option value="Anomaly Alert Investigations">Anomaly Alert Investigations</option>
+              <option value="RI/SP Coverage">RI/SP Coverage</option>
+            </select>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+
+  export default DropDown;
